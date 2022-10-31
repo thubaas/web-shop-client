4e36775083ab768c3064ce3bfc055208c9c7f49d
thubaas <thubelihlesibanda716@gmail.com>
2022-10-31 11:28:46 +0100

    Added Shared module imports

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { CategoryRoutingModule } from './category-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, CategoryRoutingModule],
+  imports: [SharedModule, CategoryRoutingModule],
 })
 export class CategoryModule {}
