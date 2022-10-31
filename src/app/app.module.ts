8bc42f1db919f677481edcf2c69fb8e460b21ed6
thubaas <thubelihlesibanda716@gmail.com>
2022-10-31 11:37:22 +0100

    Moved Category declarations to category module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { CategoryComponent } from './category/category.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { CategoryModule } from './category/category.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    CategoryComponent,
+    FooterComponent
   ],
-  imports: [AppRoutingModule, BrowserModule, FormsModule, SharedModule],
+  imports: [AppRoutingModule, BrowserModule, CategoryModule, FormsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
