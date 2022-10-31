a2c946ca14e8ef4d89ea5f79e80e0affeee14d16
thubaas <thubelihlesibanda716@gmail.com>
2022-10-31 10:58:04 +0100

    Added shared module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { CategoryComponent } from './category/category.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, CategoryComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
