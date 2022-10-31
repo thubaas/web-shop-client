e20ea9efb4c300688facd2762f0fcc916cc8ab48
thubaas <thubelihlesibanda716@gmail.com>
2022-10-31 11:57:11 +0100

    Added Category child components and services

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CategoryRoutingModule } from './category-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CategoryComponent } from './category.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryItemComponent } from './category-item/category-item.component';
+import { CategoryEditComponent } from './category-edit/category-edit.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoryComponent,
+    CategoryListComponent,
+    CategoryItemComponent,
+    CategoryEditComponent,
+  ],
   imports: [SharedModule, CategoryRoutingModule],
+  exports: [CategoryItemComponent],
 })
 export class CategoryModule {}
