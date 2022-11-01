610cb58c04160503cf4899fbafe9a0ac95f71fe9
thubaas <thubelihlesibanda716@gmail.com>
2022-11-01 08:40:34 +0100

    Updated app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CartResolverService } from './cart/cart-resolver.service';
+import { CartComponent } from './cart/cart.component';
+import { CategoryResolverService } from './category/category-resolver.service';
+import { HomeComponent } from './home/home.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ProductResolverService } from './product/product-resolver.service';
+import { WishlistResolverService } from './wishlist/wishlist-resolver.service';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    resolve: [CategoryResolverService, ProductResolverService],
+  },
+  {
+    path: 'admin/products',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+  },
+  {
+    path: 'admin/categories',
+    loadChildren: () =>
+      import('./category/category.module').then((m) => m.CategoryModule),
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    resolve: [CartResolverService],
+  },
+  {
+    path: 'wishlist',
+    component: WishlistComponent,
+    resolve: [WishlistResolverService],
+  },
+  {
+    path: 'checkout',
+    component: PaymentComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
   declarations: [],
