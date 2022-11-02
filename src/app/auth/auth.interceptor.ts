7db310af8ac2349addc9c214b2aa2b06a7f1f0ed
thubaas <thubelihlesibanda716@gmail.com>
2022-11-02 09:06:27 +0100

    Implemented auth interceptor

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -3,16 +3,34 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpParams,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { exhaustMap, take } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return this.authService.authToken.pipe(
+      take(1),
+      exhaustMap((token) => {
+        console.log('TOKEN : ', token);
+        if (!token) {
+          return next.handle(request);
+        }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+        const modifiedReq = request.clone({
+          params: new HttpParams().set('token', token),
+        });
+        return next.handle(modifiedReq);
+      })
+    );
   }
 }
