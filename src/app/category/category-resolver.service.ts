755e71569a743e2a783a1916417a85d21f0843a8
thubaas <thubelihlesibanda716@gmail.com>
2022-11-02 09:22:28 +0100

    Updated the category resolver service

--- src/app/category/category-resolver.service.ts
+++ src/app/category/category-resolver.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CategoryModel } from './category.model';
+import { CategoryService } from './category.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryResolverService {
+export class CategoryResolverService implements Resolve<CategoryModel[]> {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): CategoryModel[] | Observable<CategoryModel[]> | Promise<CategoryModel[]> {
+    throw new Error('Method not implemented.');
+  }
 }
