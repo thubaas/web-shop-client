e0f31dda7e9eafcf5f96688fd55a927fcfd5cfef
thubaas <thubelihlesibanda716@gmail.com>
2022-11-15 22:16:47 +0100

    Changed menu in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  show:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+ onShowMenu() {
+  let openBtn = document.getElementById('icon-menu');
+  let closeBtn = document.getElementById('icon-close');
+  let sidebarNavs = document.getElementById('sidebar-navs');
+  openBtn?.classList.add('item-hide')
+  closeBtn?.classList.remove('item-hide')
+  sidebarNavs?.classList.remove('item-hide')
+}
+
+onHideMenu() {
+ let openBtn = document.getElementById('icon-menu');
+ let closeBtn = document.getElementById('icon-close');
+ let sidebarNavs = document.getElementById('sidebar-navs');
+ openBtn?.classList.remove('item-hide')
+ closeBtn?.classList.add('item-hide')
+ sidebarNavs?.classList.add('item-hide')
+}
 
 }
