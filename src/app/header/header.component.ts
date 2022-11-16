9fe1e8978d9cbc5d136f581f22a67007d5646533
thubaas <thubelihlesibanda716@gmail.com>
2022-11-16 12:39:56 +0100

    Moved side menu to the app component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,36 +1,14 @@
-
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  show: boolean = false;
 
-  show:boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
- onShowMenu() {
-  let openBtn = document.getElementById('icon-menu');
-  let closeBtn = document.getElementById('icon-close');
-  let sidebarNavs = document.getElementById('sidebar-navs');
-  openBtn?.classList.add('item-hide')
-  closeBtn?.classList.remove('item-hide')
-  sidebarNavs?.classList.remove('item-hide')
-}
-
-onHideMenu() {
- let openBtn = document.getElementById('icon-menu');
- let closeBtn = document.getElementById('icon-close');
- let sidebarNavs = document.getElementById('sidebar-navs');
- openBtn?.classList.remove('item-hide')
- closeBtn?.classList.add('item-hide')
- sidebarNavs?.classList.add('item-hide')
-}
-
+  ngOnInit(): void {}
 }
